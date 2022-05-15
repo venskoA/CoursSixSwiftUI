116b7eec082f755b19012888165a88860f90ca8e
venskoA <venskoandrey@gmail.com>
2022-05-15 10:38:23 +0300

    Start project

---
+++ CourseSixSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CourseSixSwiftUI
+//
+//  Created by Andrey Vensko on 14.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CourseSixSwiftUI/CourseSixSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseSixSwiftUIApp.swift
+//  CourseSixSwiftUI
+//
+//  Created by Andrey Vensko on 14.05.22.
+//
+
+import SwiftUI
+
+@main
+struct CourseSixSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
