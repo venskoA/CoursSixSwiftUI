2a02505f4174516f32fac45bd1ed2b8e4663bb7b
venskoA <venskoandrey@gmail.com>
2022-05-15 12:43:55 +0300

    First task. Login screen layout.

--- CourseSixSwiftUI/ContentView.swift
+++ CourseSixSwiftUI/ContentView.swift
@@ -6,11 +6,96 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    private let loginDef = "Admin"
+    private let passwordDef = "1"
+
+    @State private var login: String = ""
+    @State private var password: String = ""
+    @State private var shouldShowLogo: Bool = true
+    private let keyboardIsOnPublisher = Publishers.Merge(
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .map { _ in true },
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification).map { _ in false }
+    ).removeDuplicates()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        ZStack {
+
+            GeometryReader { geometry in
+                Image("111")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(maxWidth: geometry.size.width, maxHeight: 1000)
+            }
+
+            ScrollView {
+
+                VStack {
+                    Spacer(minLength: 150)
+
+                    Text("VK")
+                        .foregroundColor(.blue)
+                        .font(.system(size: 60))
+                        .font(.system(.largeTitle))
+                        .padding(.top, 10)
+
+                    Spacer(minLength: 180)
+
+                    VStack {
+                        HStack {
+                            Text("Login:")
+                                .foregroundColor(.white)
+                            Spacer()
+                            TextField("", text: $login)
+                                .frame(maxWidth: 230)
+                                .background(.gray)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        }
+                        Spacer(minLength: 50)
+                        HStack {
+                            Text("Password:")
+                                .foregroundColor(.white)
+                            Spacer()
+                            SecureField("", text: $password)
+                                .frame(maxWidth: 230)
+                                .background(.gray)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        }
+                    }
+                    .frame(maxWidth: 350)
+
+                    Spacer(minLength: 50)
+
+                    Button {
+                        if login == loginDef && password == passwordDef {
+                            print("mkmkm")
+                        }
+                    } label: {
+                        Text("Ok")
+                            .colorMultiply(.black)
+                    }
+                    .padding()
+                    .background(.blue)
+                }
+            }
+
+            Spacer(minLength: 20)
+
+        }.onTapGesture {
+            UIApplication.shared.endEditing()
+        }
+    }
+}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from:
+                    nil, for: nil)
     }
 }
 
@@ -19,3 +104,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
