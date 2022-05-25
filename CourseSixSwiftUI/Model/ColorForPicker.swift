c39d1c7071be2352ad50d010b1603ceaf6196d6a
venskoA <venskoandrey@gmail.com>
2022-05-25 20:07:56 +0300

    create Model ColorForPicker

---
+++ CourseSixSwiftUI/Model/ColorForPicker.swift
@@ -0,0 +1,14 @@
+//
+//  ColorForPiker.swift
+//  CourseSixSwiftUI
+//
+//  Created by Andrey Vensko on 22.05.22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorForPicker: Equatable {
+    var nameColor: String
+    var color: Color
+}
